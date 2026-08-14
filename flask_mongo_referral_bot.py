f4f3ab335df48bee7fc630e548ef7e3d50c07c56
javacpmu <tojiyevjavohir547@gmail.com>
2026-08-14 17:43:05 +0500

    Update flask_mongo_referral_bot.py

--- flask_mongo_referral_bot.py
+++ flask_mongo_referral_bot.py
@@ -65,7 +65,7 @@ load_env_file()
 BOT_TOKEN = first_valid_env("BOT_TOKEN", "8831278254:AAGKA8Of31idOVRUnmNHXDx6ebFACerB_Gk", "TOKEN")
 OWNER_ID = int(clean_env("OWNER_ID", "6968399046") or "6968399046")
 ADMIN_IDS_TEXT = clean_env("ADMIN_IDS", str(OWNER_ID))
-MONGO_URI = first_valid_env("MONGODB_URI", "MONGO_URI", default="mongodb+srv://javohirtojiyev99_db_user:<javohir2011>@cluster0.4skcgpy.mongodb.net/?appName=Cluster0")
+MONGO_URI = first_valid_env("MONGODB_URI", "MONGO_URI", default="mongodb+srv://javohirtojiyev99_db_user:javohir2011@cluster0.4skcgpy.mongodb.net/?appName=Cluster0")
 MONGO_DB = first_valid_env("MONGODB_DB", "MONGO_DB", default="referral_coin_bot")
 WEBHOOK_URL = render_base_url()
 WEBHOOK_SECRET = clean_env("WEBHOOK_SECRET", "referral-secret") or "referral-secret"
